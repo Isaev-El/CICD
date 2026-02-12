0f1aee38d7eae62ed50319528e28f64d88f15a6d
Isaev-El <isaev.elkhan2003@gmail.com>
2026-02-12 15:15:48 +0500

    To new Repo

---
+++ DevOpsPractice.Tests/CalculatorTests.cs
@@ -0,0 +1,16 @@
+using DevOpsPractice.Api.Services;
+
+namespace DevOpsPractice.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        var service = new CalculatorService();
+
+        var result = service.Add(2, 3);
+
+        Assert.Equal(5, result);
+    }
+}
